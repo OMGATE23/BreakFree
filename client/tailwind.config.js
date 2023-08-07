c6f7774d891737cedc1fd0b6c0511929ef5f6eaa
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2023-08-07 20:01:32 +0530

    style : added tailwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{tsx,jsx}', './src/*.{tsx,jsx}'],
+  theme: {
+    fontFamily : {
+      lato : ['Lato'],
+      poppins : ['Poppins']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
