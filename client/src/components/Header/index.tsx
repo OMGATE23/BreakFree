e34539c31e62fa31ce0a393eff993b16187d85c3
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2023-08-16 02:02:54 +0530

    fix: img assets

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -8,7 +8,7 @@ export default function Header() {
   return (
     
     <nav className="p-4 bg-white top-0 text-2xl w-[100vw] flex justify-between items-center ">
-        <img className='block w-[48px]' src={'./public/assets/plant.png'} />
+        <img className='block w-[48px]' src={'./assets/plant.png'} />
         {isAuthenticated && <div className="text-lg flex justify-center gap-12 w-[50%]">
           <NavLink className="text-green-700" style={({isActive}) => ({cursor : isActive ? 'default' : 'pointer' , fontWeight : isActive ? 'bold' : 'normal'})}  to='/'>Timers</NavLink>
           <NavLink className="text-green-700" to='/analytics' style={({isActive}) => ({cursor : isActive ? 'default' : 'pointer' , fontWeight : isActive ? 'bold' : 'normal'})} >Analytics</NavLink>
--- client/src/components/HomeNotLoggedIn/index.tsx
+++ client/src/components/HomeNotLoggedIn/index.tsx
@@ -2,7 +2,7 @@ const HomeNotLoggedIn = () => {
   return (
     <div>
       <div className='flex flex-col items-center justify-center w-[90vw] mx-auto mb-12 gap-8 md:h-[90vh]'>
-      <img className='block w-[120px]' src={'./public/assets/plant.png'} />
+      <img className='block w-[120px]' src={'./assets/plant.png'} />
         <div className='flex flex-col gap-2 text-center'>
           <h1 className=' text-4xl  md:text-8xl font-bold text-green-900'>Break<span className='text-purple-950'>Free</span></h1>
           <h2 className=' text-xl md:text-2xl text-green-950'>Your online tool to help you with your social media addiction!</h2>
