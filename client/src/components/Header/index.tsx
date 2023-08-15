96cf8072ff687b10634cf886591ca062e913e878
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2023-08-15 19:58:19 +0530

    feat : added header

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,7 +1,20 @@
+import { useAuth0 } from "@auth0/auth0-react"
+import LoginButton from "../LoginButton"
+import { LogoutButton } from "../LogoutButton"
+import { Link, NavLink } from "react-router-dom"
+
 export default function Header() {
+  const {isAuthenticated} = useAuth0()
   return (
-    <div className="py-2 font-bold">
-        Header
-    </div>
+    
+    <nav className="p-4 bg-white top-0 text-2xl w-[100vw] flex justify-between items-center ">
+        <img className='block w-[48px]' src={'./public/assets/plant.png'} />
+        {isAuthenticated && <div className="text-lg flex justify-center gap-12 w-[50%]">
+          <NavLink className="text-green-700" style={({isActive}) => ({cursor : isActive ? 'default' : 'pointer' , fontWeight : isActive ? 'bold' : 'normal'})}  to='/'>Timers</NavLink>
+          <NavLink className="text-green-700" to='/analytics' style={({isActive}) => ({cursor : isActive ? 'default' : 'pointer' , fontWeight : isActive ? 'bold' : 'normal'})} >Analytics</NavLink>
+        </div>}
+        {!isAuthenticated && <LoginButton/>}
+      {isAuthenticated && <LogoutButton/>}
+    </nav>
   )
 }
